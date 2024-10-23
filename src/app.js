375b17fbbcb0fb10a4fda2a397c266035701ffff
AitorMoran <aitorfm8@gmail.com>
2024-10-23 16:30:06 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,36 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+//VARS -----------------------------------------------------------------------
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const suits = ["♠️", "♣️", "♦️", "♥️"];
+const SuitsColor = ["black", "red"];
+
+const card = document.querySelector("#numberCard");
+const suitUp = document.querySelector("#suitUp");
+const numberCenter = document.querySelector("#numberCenter");
+const suitDown = document.querySelector("#suitDown");
+
+//FUNCTIONS ------------------------------------------------------------------------
+
+const GetRandomNumber = numbers[Math.floor(Math.random() * numbers.length)]; //función nº random aleatorio
+const GetRandomSuit = suits[Math.floor(Math.random() * suits.length)];
+
+function printToDocument() {
+  numberCenter.textContent = GetRandomNumber;
+  suitUp.textContent = GetRandomSuit;
+  suitDown.textContent = GetRandomSuit;
+
+  if (GetRandomSuit === "♥️" || GetRandomSuit === "♦️") {
+    suitUp.style.color = "red";
+    suitDown.style.color = "red";
+  }
+}
+
+//EVENTS -------------------------------------------------------------------------
+
+window.onload = function() {
+  printToDocument();
+};
